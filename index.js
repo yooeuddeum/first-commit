f05512d3763ba2b62929fe0150ecc938913302aa
유으뜸 <www.goolge.com>
2024-05-28 13:00:53 +0900

    요청 방식 확인하고 index.html파일을 읽고 응답했음

--- index.js
+++ index.js
@@ -4,3 +4,22 @@ const fs = require("fs");
 // console.log(fs);
 const path = require("path");
 // console.log(path);
+
+const server = http.createServer((req, res) => {
+  if (req.method === "GET") {
+    if (req.url === "/") {
+      fs.readFile(path.join(__dirname, "index.html"), (err, data) => {
+        if (err) {
+          res.writeHead(500, { "Content-Type": "text/plain" });
+          res.end();
+          return;
+        }
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(data);
+      });
+    } else {
+      res.writeHead(404, {"Content-Type" : "text/plain"});
+      res.end()
+    }
+  }
+});
