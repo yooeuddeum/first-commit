bcbd8ea11a17ae1f143afcf060c4ea22d5f66f3f
유으뜸 <www.goolge.com>
2024-05-29 16:32:33 +0900

    로컬상 적용해보기

---
+++ script.js
@@ -0,0 +1,6 @@
+const noteForm = document.querySelector("#note-form");
+console.log(noteForm);
+const viewNotes = document.querySelector("#view-notes");
+console.log(viewNotes);
+const notesList = document.querySelector("#notes-list");
+console.log(notesList);
