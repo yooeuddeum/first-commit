79f74f8e9122fd9bc61d44e364fdc27ef336102f
유으뜸 <www.goolge.com>
2024-05-31 12:45:28 +0900

    branch 메인과 병합

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const path = require("path");
 const server = http.createServer((req, res) => {
   if (req.method === "GET") {
     if (req.url === "/") {
-      fs.readFile(path.join(__dirname, "index.html"), (err, data) => {
+      fs.readFile(path.join(__dirname, "index2.html"), (err, data) => {
         if (err) {
           res.writeHead(500, { "Content-Type": "text/plain" });
           res.end();
